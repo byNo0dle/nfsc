8daa5b27aea1610d73a561110d06c0e48dc29ae2
Nick Saurino <nicksaurinocruz@gmail.com>
2024-05-14 21:29:27 +0200

    The attributes of the products class were added and the lombok dependency was implemented

--- src/main/java/com/ufostyle/cp/domain/entities/Products.java
+++ src/main/java/com/ufostyle/cp/domain/entities/Products.java
@@ -1,4 +1,15 @@
 package com.ufostyle.cp.domain.entities;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "products")
 public class Products {
+
+    @Id
+    private Long codeProduct;
+    private String name;
+    private Double price;
 }
